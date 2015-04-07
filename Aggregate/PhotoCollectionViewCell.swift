ee1e1d73e1c6fce4e704efd9e3f04eaddba0c028
Sophie Smith <ladylowlatency@gmail.com>
2015-04-06 17:59:23 -0700

    initial commit

---
+++ Aggregate/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Aggregate
+//
+//  Created by Sophie on 4/6/15.
+//  Copyright (c) 2015 Sophie. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ Aggregate/PhotosCollectionViewController.swift
@@ -0,0 +1,84 @@
+//
+//  PhotosCollectionViewController.swift
+//  Aggregate
+//
+//  Created by Sophie on 4/6/15.
+//  Copyright (c) 2015 Sophie. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "photoCell"
+
+class PhotosCollectionViewController: UICollectionViewController, UITextFieldDelegate, UICollectionViewDelegateFlowLayout {
+    private var searches = [FlickrSearchResults]()
+    private let flickr = Flickr()
+    
+    func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    func collectionView(collectionView: UICollectionView!,
+        layout collectionViewLayout: UICollectionViewLayout!,
+        sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+            
+            let flickrPhoto =  photoForIndexPath(indexPath)
+            if var size = flickrPhoto.thumbnail?.size {
+                size.width += 10
+                size.height += 10
+                return size
+            }
+            return CGSize(width: 100, height: 100)
+    }
+    
+    func collectionView(collectionView: UICollectionView!,
+        layout collectionViewLayout: UICollectionViewLayout!,
+        insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    }
+    
+    // MARK: UICollectionViewDataSource
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as PhotoCollectionViewCell
+        let flickrPhoto = photoForIndexPath(indexPath)
+        cell.imageView.image = flickrPhoto.thumbnail
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+    
+    // MARK : UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        flickr.searchFlickrForTerm(textField.text) {
+            results, error in
+            
+            activityIndicator.removeFromSuperview()
+            if error != nil {
+                println("Error searching : \(error)")
+            }
+            
+            if results != nil {
+                println("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+                self.searches.insert(results!, atIndex: 0)
+                
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
